bf45fea8073f3286ddf43acc66cfc432b0a0a4f0
Pauline Rudge <paulinerudge@me.com>
2019-04-02 20:31:08 +0100

    park test runs

--- src/main/java/Attractions/Dodgems.java
+++ src/main/java/Attractions/Dodgems.java
@@ -1,12 +1,23 @@
 package Attractions;
 
 import Attractions.Attraction;
+import ThemePark.ISecurity;
+import ThemePark.ITicketed;
+import ThemePark.Visitor;
 
-public class Dodgems extends Attraction {
+public class Dodgems extends Attraction implements ITicketed {
 
     public Dodgems(String name){
         super(name);
     }
 
+
+
+    public double priceForVisitor(Visitor visitor){
+      if (visitor.getAge() < 12){
+        return visitor.priceForVisitor(visitor) * 1/2;
+      }
+        return visitor.priceForVisitor(visitor);
+      }
 
 }
--- src/test/java/DodgemsTest.java
+++ src/test/java/DodgemsTest.java
@@ -1,11 +1,13 @@
 import Attractions.Dodgems;
 
+import ThemePark.Visitor;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class DodgemsTest {
     private Dodgems dodgems;
+    private Visitor visitor;
 
     @Before
     public void before() {
@@ -16,5 +18,18 @@ public class DodgemsTest {
     public void canGetName() {
         assertEquals("Bumper Cars", dodgems.getName());
 
+    }
+
+    @Test
+    public void getPriceForVisitor(){
+        visitor = new Visitor(34,1.30, 15);
+        assertEquals(10.00, dodgems.priceForVisitor(visitor), 2);
+
+    }
+
+    @Test
+    public void halvesFeeUnder12(){
+        visitor = new Visitor(11,2.30, 15);
+        assertEquals(5.00, dodgems.priceForVisitor(visitor), 2);
     }
 }
