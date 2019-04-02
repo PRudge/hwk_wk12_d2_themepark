b5968cd81c3732c0b97cc1337b8dcdb560a121b8
Pauline Rudge <paulinerudge@me.com>
2019-04-02 21:52:49 +0100

    added age 18 to tobacco

--- src/main/java/Stalls/TobaccoStall.java
+++ src/main/java/Stalls/TobaccoStall.java
@@ -19,7 +19,7 @@ public class TobaccoStall extends Stall implements ISecurity {
     }
 
     public boolean IsAllowed(Visitor visitor){
-        if (visitor.getAge() > 15 ) {
+        if (visitor.getAge() > 18 ) {
             return true;
         }
         return false;
